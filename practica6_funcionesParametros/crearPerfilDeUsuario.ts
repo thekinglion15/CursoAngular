932be717640f49c0ea36704b0aaa3bfb74a2a897
The King Lion <kinglionkl250@gmail.com>
2024-03-26 19:30:09 -0400

    Practica 6 Funciones con Parámetros Opcionales y Predeterminados

---
+++ practica6_funcionesParametros/crearPerfilDeUsuario.ts
@@ -0,0 +1,20 @@
+//Gabriel Alexander De León Lizardo
+//Interface para crear el objeto
+interface PerfilDeUsuario {
+    nombre: string;
+    edad: number;
+    pais: string;
+}
+
+//Función para crear el perfil
+function crearPerfilDeUsuario(nombre: string, edad: number, pais: string = "No especificado"): PerfilDeUsuario {
+    const perfil: PerfilDeUsuario = {nombre, edad, pais}
+    return perfil;
+}
+
+//Ejemplos
+const perfil1 = crearPerfilDeUsuario("Enmanuel", 27, "República Dominicana");
+const perfil2 = crearPerfilDeUsuario("Cekate turaja", 32);
+
+console.log(perfil1);
+console.log(perfil2);
